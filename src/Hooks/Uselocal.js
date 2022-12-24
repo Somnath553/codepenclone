eb5fb0cfed7e2c2c0fa9ba914a865fc6e92fbb11
Somnath553 <somnath553.in@gmail.com>
2022-12-25 00:49:40 +0530

    secondt commit

--- src/Hooks/Uselocal.js
+++ src/Hooks/Uselocal.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import {useEffect,useState} from 'react'
 const PREFIX='clone'
 function Uselocal(key,initialValue) {
     const prefixedKey=PREFIX+key
